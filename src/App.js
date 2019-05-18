a05f8875c3acf7b34f96db77c0c4f0a782e9aec2
mattperkins <github@mattperkins.com>
2019-05-18 22:24:53 +0100

    add provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { Provider } from 'react-redux'
+import { store } from './redux'
+
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
 function App() {
   return (
-    <div>
-      <h1>hello,world</h1>
-    </div>
+    <Provider store={store}>
+      <div className="main">
+        <TodoInput />
+        <TodoList />
+      </div>
+    </Provider>
   );
 }
 
