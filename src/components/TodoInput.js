1c2ee04f5050c2ee370dd4fc7ee3c94e9b133bf1
mattperkins <github@mattperkins.com>
2019-05-18 23:31:43 +0100

    cl

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import uuid from 'uuid/v4';
+import { addTodoAction } from '../redux';
+import { useDispatch } from 'react-redux';
 
-export default function TodoInput() {
+const TodoInput = () => {
+  const [todo, setTodo] = useState('');
+  const dispatch = useDispatch();
+  const addTodo = (todo) => dispatch(addTodoAction(todo));
+
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo.trim() === '') return;
+    addTodo({
+      id: uuid(),
+      name: todo,
+      complete: false
+    });
+    setTodo('');
+  };
+
   return (
-    <h1>TodoInput</h1>
-  )
-}
+    <form onSubmit={onSubmit}>
+      <div className="form-div">
+        <input
+          type="text"
+          name="todo"
+          placeholder="Add a todo"
+          value={todo}
+          onChange={onChange}
+        />
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default TodoInput;
